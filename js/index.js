f8e5bfd534d143c0272ae41c39d44631954dd411
Joy Lin <vvlnote@hotmail.com>
2019-06-23 20:52:16 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,3 +5,10 @@ element.innerHTML = "Hello, DOM!";
 element.style.background = "#f9f9f9";
 document.body.appendChild(element);
 element.style.textAlign = 'center';
+
+let ul = document.createElement('ul');
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i+1).toString();
+  ul.appendChild(li);
+}
