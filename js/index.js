e7fae6de3d387b3bc35dcb6e317a20935b0433d6
Joy Lin <vvlnote@hotmail.com>
2019-06-23 20:55:19 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -8,9 +8,10 @@ element.style.textAlign = 'center';
 
 let ul = document.createElement('ul');
 for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li');
-  li.innerHTML = (i+1).toString();
-  ul.appendChild(li);
+    let li = document.createElement('li');
+    li.innerHTML = (i+1).toString();
+    ul.appendChild(li);
+  }
+  element.appendChild(ul);
   ul.style.textAlign = "left";
 }
-element.appendChild(ul);
