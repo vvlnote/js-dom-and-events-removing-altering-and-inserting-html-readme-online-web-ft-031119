5c38942f525e83b05d707b963e2a96101a8299ca
Joy Lin <vvlnote@hotmail.com>
2019-06-23 20:49:53 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+let element = document.createElement('div');
+element.innerHTML = "Hello, DOM!";
+element.style.background = "#f9f9f9";
+document.body.appendChild(element);
